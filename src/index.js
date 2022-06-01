131e359f658f312b35ed6462c7205ee56a4a33d7
Georges Bueno <georges.bueno@hotmail.com>
2022-05-31 20:15:03 -0400

    Se creo el login

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hola mundo</h1>
     <Login />
   </React.StrictMode>
 );
