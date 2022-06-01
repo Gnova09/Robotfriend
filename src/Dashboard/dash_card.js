221d8e97052eafe80d1ca805e4cdde50b744e7fd
Georges Bueno <georges.bueno@hotmail.com>
2022-06-01 18:49:43 -0400

    update dash with props

--- src/Dashboard/dash_card.js
+++ src/Dashboard/dash_card.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 const dash_card = (props) =>{
+const {name, email} = props;
 
     return(
         
             <div className = ' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
                 <img alt="photo" src={`https://robohash.org/${props.name}`}/>
                 <div>
-                    <h2>{props.name}</h2>
-                    <p>{props.email}</p>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
                 </div>
             </div>
         
