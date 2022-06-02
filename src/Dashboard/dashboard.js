c3de440db2d68fffe627950d1b99d9fe5e587939
Georges Bueno <georges.bueno@hotmail.com>
2022-06-02 02:21:31 -0400

    se agrego el toggle al sidebar

--- src/Dashboard/dashboard.js
+++ src/Dashboard/dashboard.js
@@ -1,41 +1,61 @@
-import React from "react";
+import React, {Component} from "react";
 import './dashboard.css';
+import {names} from "./names.js";
 import CardComponent from './cardComponent';
 
 
-const dashboard = ({props}) => {
-    return (
-        <div className ='body'>
-            {/* SECCION DEL SIDEBAR */}
-            <div className ='sidebar active'>
-                <ul>
-                    <li className ='logo'>
-                        <img className ='avatar' src={require('../img/logo.png')} alt="logo"/>
-                        <p>Nombre Empresa</p>
-                    </li>
-                    <li>
-                        <img src={require('../icons/new-user.png')} alt="Add user"/>
-                        <p> Agregar Usuario</p> </li>
-                    <li>
-                        <img src={require('../icons/lista.png')} alt="Add user"/> 
-                        <p>Lista de Usuarios</p> </li>
-                </ul>
-            </div>  
+class dashboard extends Component {
+    constructor(){
+        super()
+        this.state = {
+            props_name: names,
+            btn_sidebar: ''
+        }
+    }
+    onclick_btnsidebar(event){
+        const side = document.getElementById("side");
 
-            {/* SECCION DEL HEADER Y COMPONENTES  */}
-            
-             <div className ='contenedor'>
-                <div className ='superiobar'>
-                    <div>
-                        <span className ='sidebar-btn' >&#9776;</span>
+        if (side.classList.contains('close')) {
+          side.classList.replace('close', 'active');
+        } else {
+            side.classList.replace('active', 'close');
+        }
+    }
+    render(){
+        this.setState(this.state);
+        return (
+            <div className ='body'>
+                {/* SECCION DEL SIDEBAR */}
+                <div id="side"className ='sidebar close'>
+                    <ul>
+                        <li className ='logo'>
+                            <img className ='avatar' src={require('../img/logo.png')} alt="logo"/>
+                            <p>Nombre Empresa</p>
+                        </li>
+                        <li>
+                            <img src={require('../icons/new-user.png')} alt="Add user"/>
+                            <p> Agregar Usuario</p> </li>
+                        <li>
+                            <img src={require('../icons/lista.png')} alt="Add user"/> 
+                            <p>Lista de Usuarios</p> </li>
+                    </ul>
+                </div>  
+
+                {/* SECCION DEL HEADER Y COMPONENTES  */}
+                
+                <div className ='contenedor'>
+                    <div className ='superiobar'>
+                        <div>
+                            <span onClick={this.onclick_btnsidebar} className ='sidebar-btn' >&#9776;</span>
+                        </div>
                     </div>
-                </div>
 
-                <div className ='card tc pa3'>
-                   <CardComponent names={props} /> 
-                </div>
-            </div>       
-        </div>
-    );
+                    <div className ='card tc pa3'>
+                    <CardComponent names={this.state.props_name} /> 
+                    </div>
+                </div>       
+            </div>
+        );
+    }
 } 
 export default dashboard;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
-import App from './App';
+import Dashboard from './Dashboard/dashboard';
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Dashboard/>
   </React.StrictMode>
 );
 
